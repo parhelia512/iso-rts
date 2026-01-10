95e9383d31d77f1b8685919174b0e30d170a51ad
Davide Coppola <vivaladav@gmail.com>
2026-01-10 20:29:06 +0100

    Adapt PanelShotType to multi language

--- src/Widgets/PanelShotType.cpp
+++ src/Widgets/PanelShotType.cpp
@@ -1,10 +1,12 @@
 #include "Widgets/PanelShotType.h"
 
+#include "GameConstants.h"
 #include "Widgets/ButtonObjectActionOption.h"
 
 #include <sgl/core/event/KeyboardEvent.h>
 #include <sgl/media/AudioManager.h>
 #include <sgl/media/AudioPlayer.h>
+#include <sgl/utilities/StringManager.h>
 
 namespace game
 {
@@ -15,29 +17,33 @@ PanelShotType::PanelShotType()
 {
     using namespace sgl;
 
+    auto sm = utilities::StringManager::Instance();
+    sm->AddListener(this);
+
     // spacing between buttons
     const int spacing = 10;
     SetSpacing(spacing);
 
     // BUTTON QUICK SHOT
     const int tooltipTime1 = 3000;
-    auto btn = new ButtonObjectActionOption("QUICK SHOT", "1", core::KeyboardEvent::KEY_1, this);
-    btn->CreateTooltip("Low energy cost, but lower accuracy", tooltipTime1);
+    auto btn = new ButtonObjectActionOption(sm->GetCString("QUICK_SHOT"),"1",
+                                            core::KeyboardEvent::KEY_1, this);
+    btn->CreateTooltip(sm->GetCString("TT_QUICK_SHOT"), tooltipTime1);
 
     AddButton(btn);
 
     // BUTTON AIMED SHOT
     const int tooltipTime2 = 4000;
-    btn = new ButtonObjectActionOption("AIMED SHOT", "2", core::KeyboardEvent::KEY_2, this);
-    btn->CreateTooltip("High energy cost, but high accuracy and higher probability of fatal shot",
-                        tooltipTime2);
+    btn = new ButtonObjectActionOption(sm->GetCString("AIMED_SHOT"), "2",
+                                       core::KeyboardEvent::KEY_2, this);
+    btn->CreateTooltip(sm->GetCString("TT_AIMED_SHOT"), tooltipTime2);
 
     AddButton(btn);
 
     // BUTTON BURST SHOT
-    btn = new ButtonObjectActionOption("BURST", "3", core::KeyboardEvent::KEY_3, this);
-    btn->CreateTooltip("Multiple shots, but low accuracy",
-                        tooltipTime1);
+    btn = new ButtonObjectActionOption(sm->GetCString("BURST_SHOT"), "3",
+                                       core::KeyboardEvent::KEY_3, this);
+    btn->CreateTooltip(sm->GetCString("TT_BURST_SHOT"), tooltipTime1);
     AddButton(btn);
 
     SetButtonChecked(0, true);
@@ -54,4 +60,20 @@ PanelShotType::~PanelShotType()
     ap->PlaySound("UI/panel-close-01.ogg");
 }
 
+void PanelShotType::OnStringsChanged()
+{
+    auto sm = sgl::utilities::StringManager::Instance();
+
+    auto btn = static_cast<ButtonObjectActionOption*>(GetButton(ATT_QUICK_SHOT));
+    btn->SetText(sm->GetCString("QUICK_SHOT"));
+    btn->SetTooltipText(sm->GetCString("TT_QUICK_SHOT"));
+
+    btn = static_cast<ButtonObjectActionOption*>(GetButton(ATT_AIMED_SHOT));
+    btn->SetText(sm->GetCString("AIMED_SHOT"));
+    btn->SetTooltipText(sm->GetCString("TT_AIMED_SHOT"));
+
+    btn = static_cast<ButtonObjectActionOption*>(GetButton(ATT_BURST_SHOT));
+    btn->SetText(sm->GetCString("BURST_SHOT"));
+    btn->SetTooltipText(sm->GetCString("TT_BURST_SHOT"));
+}
 } // namespace game
--- src/Widgets/PanelShotType.h
+++ src/Widgets/PanelShotType.h
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <sgl/sgui/ButtonsGroup.h>
+#include <sgl/utilities/StringsChangeListener.h>
 
 namespace game
 {
 
-class PanelShotType : public sgl::sgui::ButtonsGroup
+class PanelShotType : public sgl::sgui::ButtonsGroup,
+                      public sgl::utilities::StringsChangeListener
 {
 public:
     PanelShotType();
     ~PanelShotType();
+
+private:
+    void OnStringsChanged() override;
 };
 
 } // namespace game
